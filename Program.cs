21344e9089560ea87e518f03d616219a28a373bf
Ryan Youngblood <rryb77@gmail.com>
2021-03-04 15:22:13 -0600

    Translated the JS Wish List project to C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace wishlist
 {
@@ -6,7 +7,22 @@ namespace wishlist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> iWishFor = new List<string>()
+            {
+                "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'",
+                "The ability to think of interesting wishes on short notice",
+                "A washtub of Skittles",
+                "World peace, but not the kind of world peace where all the humans are removed from existence",
+                "A TV that is NOT internet-capable",
+                "The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"
+            };
+
+            Console.WriteLine("My wishes...");
+
+            foreach (var wish in iWishFor)
+            {
+                Console.WriteLine(wish);
+            };
         }
     }
 }
